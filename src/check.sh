d17a61ffca5c39ab6bc22c9c9f42e70285ac9704
Gary Young <proudindiv@gmail.com>
2016-07-16 22:46:22 -0600

    updates to Apollo-11.nc etc from chrislgarry, plus some he should have

--- src/check.sh
+++ src/check.sh
@@ -1,4 +1,4 @@
-orig=Luminary099/AGC_BLOCK_TWO_SELF_CHECK.agc; new=AGC-BLOCK-TWO-SELF--CHECK.agc; echo "XX $orig XX"; diff $orig src/Luminary099/$new
+orig=Luminary099/AGC_BLOCK_TWO_SELF_CHECK.agc; new=AGC-BLOCK-TWO-SELF-CHECK.agc; echo "XX $orig XX"; diff $orig src/Luminary099/$new
 orig=Comanche055/AGC_BLOCK_TWO_SELF-CHECK.agc; new=AGC-BLOCK-TWO-SELF-CHECK.agc; echo "XX $orig XX"; diff $orig src/Comanche055/$new
 orig=Luminary099/AGS_INITIALIZATION.agc; new=AGS-INITIALIZATION.agc; echo "XX $orig XX"; diff $orig src/Luminary099/$new
 orig=Comanche055/ALARM_AND_ABORT.agc; new=ALARM-AND-ABORT.agc; echo "XX $orig XX"; diff $orig src/Comanche055/$new
